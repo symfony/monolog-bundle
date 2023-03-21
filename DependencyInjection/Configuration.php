81c547c17d3af4fd1e027fb201475821476be826
HypeMC <hypemc@gmail.com>
2023-03-21 08:56:58 +0100

    Add handler_default_channels configuration option

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -383,6 +383,8 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder('monolog');
         $rootNode = method_exists(TreeBuilder::class, 'getRootNode') ? $treeBuilder->getRootNode() : $treeBuilder->root('monolog');
 
+        $this->addChannelsSection($rootNode, 'handler_default_channels');
+
         $handlers = $rootNode
             ->fixXmlConfig('channel')
             ->fixXmlConfig('handler')
@@ -621,6 +623,7 @@ class Configuration implements ConfigurationInterface
                 ->end()
                 ->scalarNode('formatter')->end()
                 ->booleanNode('nested')->defaultFalse()->end()
+                ->booleanNode('use_default_channels')->defaultTrue()->end()
             ->end();
 
         $this->addGelfSection($handlerNode);
@@ -793,9 +796,9 @@ class Configuration implements ConfigurationInterface
         return $treeBuilder;
     }
 
-    private function addGelfSection(ArrayNodeDefinition $handerNode)
+    private function addGelfSection(ArrayNodeDefinition $handlerNode)
     {
-        $handerNode
+        $handlerNode
             ->children()
                 ->arrayNode('publisher')
                     ->canBeUnset()
@@ -824,9 +827,9 @@ class Configuration implements ConfigurationInterface
         ;
     }
 
-    private function addMongoSection(ArrayNodeDefinition $handerNode)
+    private function addMongoSection(ArrayNodeDefinition $handlerNode)
     {
-        $handerNode
+        $handlerNode
             ->children()
                 ->arrayNode('mongo')
                     ->canBeUnset()
@@ -864,9 +867,9 @@ class Configuration implements ConfigurationInterface
         ;
     }
 
-    private function addElasticsearchSection(ArrayNodeDefinition $handerNode)
+    private function addElasticsearchSection(ArrayNodeDefinition $handlerNode)
     {
-        $handerNode
+        $handlerNode
             ->children()
                 ->arrayNode('elasticsearch')
                     ->canBeUnset()
@@ -896,9 +899,9 @@ class Configuration implements ConfigurationInterface
         ;
     }
 
-    private function addRedisSection(ArrayNodeDefinition $handerNode)
+    private function addRedisSection(ArrayNodeDefinition $handlerNode)
     {
-        $handerNode
+        $handlerNode
             ->children()
                 ->arrayNode('redis')
                     ->canBeUnset()
@@ -929,9 +932,9 @@ class Configuration implements ConfigurationInterface
         ;
     }
 
-    private function addPredisSection(ArrayNodeDefinition $handerNode)
+    private function addPredisSection(ArrayNodeDefinition $handlerNode)
     {
-        $handerNode
+        $handlerNode
             ->children()
                 ->arrayNode('predis')
                     ->canBeUnset()
@@ -958,9 +961,9 @@ class Configuration implements ConfigurationInterface
         ;
     }
 
-    private function addMailerSection(ArrayNodeDefinition $handerNode)
+    private function addMailerSection(ArrayNodeDefinition $handlerNode)
     {
-        $handerNode
+        $handlerNode
             ->children()
                 ->scalarNode('from_email')->end() // swift_mailer, native_mailer, symfony_mailer and flowdock
                 ->arrayNode('to_email') // swift_mailer, native_mailer and symfony_mailer
@@ -1005,9 +1008,9 @@ class Configuration implements ConfigurationInterface
         ;
     }
 
-    private function addVerbosityLevelSection(ArrayNodeDefinition $handerNode)
+    private function addVerbosityLevelSection(ArrayNodeDefinition $handlerNode)
     {
-        $handerNode
+        $handlerNode
             ->children()
                 ->arrayNode('verbosity_levels') // console
                     ->beforeNormalization()
@@ -1071,11 +1074,11 @@ class Configuration implements ConfigurationInterface
         ;
     }
 
-    private function addChannelsSection(ArrayNodeDefinition $handerNode)
+    private function addChannelsSection(ArrayNodeDefinition $handlerNode, string $nodeName = 'channels')
     {
-        $handerNode
+        $handlerNode
             ->children()
-                ->arrayNode('channels')
+                ->arrayNode($nodeName)
                     ->fixXmlConfig('channel', 'elements')
                     ->canBeUnset()
                     ->beforeNormalization()
@@ -1091,7 +1094,7 @@ class Configuration implements ConfigurationInterface
                         ->thenUnset()
                     ->end()
                     ->validate()
-                        ->always(function ($v) {
+                        ->always(function ($v) use ($nodeName) {
                             $isExclusive = null;
                             if (isset($v['type'])) {
                                 $isExclusive = 'exclusive' === $v['type'];
@@ -1101,13 +1104,13 @@ class Configuration implements ConfigurationInterface
                             foreach ($v['elements'] as $element) {
                                 if (0 === strpos($element, '!')) {
                                     if (false === $isExclusive) {
-                                        throw new InvalidConfigurationException('Cannot combine exclusive/inclusive definitions in channels list.');
+                                        throw new InvalidConfigurationException(sprintf('Cannot combine exclusive/inclusive definitions in %s list.', $nodeName));
                                     }
                                     $elements[] = substr($element, 1);
                                     $isExclusive = true;
                                 } else {
                                     if (true === $isExclusive) {
-                                        throw new InvalidConfigurationException('Cannot combine exclusive/inclusive definitions in channels list');
+                                        throw new InvalidConfigurationException(sprintf('Cannot combine exclusive/inclusive definitions in %s list', $nodeName));
                                     }
                                     $elements[] = $element;
                                     $isExclusive = false;
@@ -1126,7 +1129,7 @@ class Configuration implements ConfigurationInterface
                         ->scalarNode('type')
                             ->validate()
                                 ->ifNotInArray(['inclusive', 'exclusive'])
-                                ->thenInvalid('The type of channels has to be inclusive or exclusive')
+                                ->thenInvalid(sprintf('The type of %s has to be inclusive or exclusive', $nodeName))
                             ->end()
                         ->end()
                         ->arrayNode('elements')
