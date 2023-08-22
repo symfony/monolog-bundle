4740a80a8713131b22024961cef8d13816ca1d76
Nicolas Grekas <nicolas.grekas@gmail.com>
2023-08-22 18:18:33 +0200

    Wire DebugLoggerConfigurator on Symfony 6.4+

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -375,10 +375,8 @@ class Configuration implements ConfigurationInterface
 {
     /**
      * Generates the configuration tree builder.
-     *
-     * @return \Symfony\Component\Config\Definition\Builder\TreeBuilder The tree builder
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('monolog');
         $rootNode = method_exists(TreeBuilder::class, 'getRootNode') ? $treeBuilder->getRootNode() : $treeBuilder->root('monolog');
