0c10c52481eb33d34153c91a6c8643b1944dbc68
Jérôme Tamarelle <jerome@tamarelle.net>
2025-12-07 23:57:47 +0100

    Use conditional class name depending on the Monolog version

--- src/DependencyInjection/MonologExtension.php
+++ src/DependencyInjection/MonologExtension.php
@@ -930,10 +930,10 @@ class MonologExtension extends Extension
                 } else {
                     $config = $handler['config'] ?: [];
                     $config['access_token'] = $handler['token'];
-                    $rollbar = new Definition('Rollbar\RollbarLogger', [
+                    $rollbar = new Definition(Logger::API === 1 ? 'RollbarNotifier' : 'Rollbar\RollbarLogger', [
                         $config,
                     ]);
-                    $rollbarId = 'monolog.rollbar.logger.'.sha1(json_encode($config));
+                    $rollbarId = 'monolog.rollbar.notifier.'.sha1(json_encode($config));
                     $rollbar->setPublic(false);
                     $container->setDefinition($rollbarId, $rollbar);
                 }
